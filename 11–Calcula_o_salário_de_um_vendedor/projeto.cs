0dc85bf4463f263e4b3e6a7dccfe5ee3623fbf68
MARCO AURÉIO DA SILVA <68288453+marco82silva@users.noreply.github.com>
2020-07-28 10:55:59 -0300

    commit

--- 11–Calcula_o_salário_de_um_vendedor/projeto.cs
+++ 11–Calcula_o_salário_de_um_vendedor/projeto.cs
@@ -3,6 +3,21 @@ using System;
 class projeto{
 
     static void Main(){
-        
+        int cod_funcionario;
+        double salario_fixo, total_vendas, percentual_ganho, salario_total;
+
+        Console.Write("Informe o codigo do Funcionario: ");
+        cod_funcionario = Int32.Parse (Console.ReadLine());
+        Console.Write("Informe o salario fixo do funcionario: ");
+        salario_fixo = double.Parse (Console.ReadLine());
+        Console.Write("Informe o valor total de vendas do funcionario: ");
+        total_vendas = double.Parse (Console.ReadLine());
+        Console.Write("Informe o valor do percentual ganho sobre o total das vendas: ");
+        percentual_ganho = double.Parse(Console.ReadLine());
+
+        salario_total= (salario_fixo+(total_vendas*(percentual_ganho/100)));
+
+        Console.Write("O funcionario de Codigo nº {0} tem salario total de: {1}",cod_funcionario,salario_total);
+
     }
 }
